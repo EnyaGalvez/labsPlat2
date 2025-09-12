9ecd86406a3661c37b6f77a580c6399d119ea4f5
EnyaGalvez <galvezenya@gmail.com>
2025-09-12 13:39:49 -0600

    configuracion inicial

---
+++ labsRM/app/src/main/java/com/example/labs_rm/characters/MainScaffold.kt
@@ -0,0 +1,17 @@
+package com.example.labs_rm.characters
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScaffold(
+    title: String,
+
+) {
+
+}
---
+++ labsRM/app/src/main/java/com/example/labs_rm/login/LoginLogic.kt
@@ -0,0 +1,4 @@
+package com.example.labs_rm.login
+
+class LoginLogic {
+}
